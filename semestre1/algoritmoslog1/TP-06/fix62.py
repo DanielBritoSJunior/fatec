c1ef969e52b9aa3ff8dbcd27821b58ed3f36e2d7
DanielBritoSJunior <danielbritodasilvajunior@gmail.com>
2024-05-20 08:52:44 -0300

    alterações aleatórias

--- semestre1/algoritmoslog1/TP-06/fix62.py
+++ semestre1/algoritmoslog1/TP-06/fix62.py
@@ -9,4 +9,3 @@ while True:
 
 with open("C:/Users/1051392411007/Desktop/fatec/semestre1/algoritmoslog1/TP-06/email.txt", "a+", encoding="utf-8") as doc:
     print(doc.read()) #printar oque esta ecrito no documento .txt
-
